bf1955e3d91c0b5b45a83dd639279e56ebdae2de
gustavoLima4 <gustavolll15567@gmail.com>
2024-06-12 20:38:38 -0300

    adicionado o card skills  com animação

--- js/script.js
+++ js/script.js
@@ -7,8 +7,7 @@ window.onscroll = function () {
   }
 };
 
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".menu-burger");
   const navLinks = document.querySelector(".nav-links");
 
@@ -17,14 +16,12 @@ document.addEventListener("DOMContentLoaded", function() {
     burger.classList.toggle("toggle");
   });
 
- 
-  navLinks.querySelectorAll("a").forEach(link => {
+  navLinks.querySelectorAll("a").forEach((link) => {
     link.addEventListener("click", () => {
       navLinks.classList.remove("active");
       burger.classList.remove("toggle");
     });
   });
-
 
   document.addEventListener("click", (event) => {
     const isClickInsideNav = navLinks.contains(event.target);
@@ -36,7 +33,37 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+const maximoDG = (event) => {
+  let input = event.target;
+  input.value = mascaratelefone(input.value);
+};
 
+const mascaratelefone = (value) => {
+  if (!value) return "";
+  value = value.replace(/\D/g, "");
+  value = value.replace(/(\d{2})(\d)/, "($1) $2");
+  value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+  return value;
+};
 
+const CaixaDeNome = document.querySelector("#nome");
 
+CaixaDeNome.addEventListener("keypress", function (e) {
+  const keyCode = e.keyCode ? e.keyCode : e.wich;
 
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
+
+document
+  .querySelector(".linguagens")
+  .addEventListener("mouseleave", function () {
+    this.querySelectorAll("li").forEach(function (li) {
+      li.style.opacity = "0";
+      li.style.animation = "none";
+      setTimeout(() => {
+        li.style.animation = "";
+      }, 10);
+    });
+  });
